d693ccb2dca2f5bf0195c2668a5631240959dc75
Takashi <Takashi@gmail.com>
2019-10-22 17:46:09 +0900

    リファクタリングを行う

--- js/main.js
+++ js/main.js
@@ -4,19 +4,8 @@
   const btn = document.getElementById('button');
 
   btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
-//    btn.textContet = n;
-
-    switch (n) {
-      case 0:
-        btn.textContent = '大吉';
-        break;
-      case 1:
-        btn.textContent = '中吉';
-        break;
-      case 2:
-        btn.textContent = '凶';
-        break;
-    }
+    const results = ['大吉', '中吉', '凶', '末吉'];
+    const n = Math.floor(Math.random() * 4);
+    btn.textContent = results[Math.floor(Math.random() * 4)];
   });
 }
