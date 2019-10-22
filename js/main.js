d3cd59e567b4acfbd7fe81803dd8e6ef7d92ff15
Takashi <Takashi@gmail.com>
2019-10-22 18:02:38 +0900

    おみくじの確率を変更する。

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@
   const btn = document.getElementById('button');
 
   btn.addEventListener('click', () => {
-    const results = ['大吉', '中吉', '凶', '末吉'];
-    const n = Math.floor(Math.random() * 4);
-    btn.textContent = results[Math.floor(Math.random() * 4)];
+    // const results = ['大吉', '中吉', '凶', '末吉'];
+    // const results = ['大吉', '大吉', '凶', '大吉'];
+    // const n = Math.floor(Math.random() * 4);
+    // btn.textContent = results[Math.floor(Math.random() * 4)];
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '中吉';
+    } else {
+      btn.textContent = '凶';
+    }
   });
 }
